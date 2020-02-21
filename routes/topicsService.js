0205b7f7db77fc4688c0b0b4e22492861961c765
Niina Siitari <siitari.niina@gmail.com>
2020-02-21 15:24:50 +0200

    Alustettu topicsService yhteysastetuksilla tietokantaan (dotenv ja pool/yhteysallas käyttöön).

--- routes/topicsService.js
+++ routes/topicsService.js
@@ -1,1 +1,13 @@
 require('dotenv').config;
+const salasana = process.env.PGPASSWORD;
+const pguser = process.env.PGUSER;
+
+const conopts = {
+    user: pguser,
+    password: salasana,
+    host: 'localhost',
+    database: 'kurssi'
+}
+
+const Pool = require('pg').Pool;
+const allas = new Pool(conopts);
