998b58dfcbd61b03313990dc241cf1a7e4690db9
Niina Siitari <siitari.niina@gmail.com>
2020-02-21 15:32:05 +0200

    topicsService lisätty GET / SELECT * from pyyntö tietokantaan.

--- routes/topicsService.js
+++ routes/topicsService.js
@@ -1,13 +1,24 @@
 require('dotenv').config;
 const salasana = process.env.PGPASSWORD;
 const pguser = process.env.PGUSER;
+const pgdatabase = process.env.PGDATABASE;
+const pghost = process.env.PGHOST;
 
 const conopts = {
     user: pguser,
     password: salasana,
-    host: 'localhost',
-    database: 'kurssi'
+    host: pghost,
+    database: pgdatabase
 }
 
 const Pool = require('pg').Pool;
 const allas = new Pool(conopts);
+
+const getTopics = (callback) => {
+    allas.query("SELECT * FROM topics", (error, data) => {
+        console.dir(data.rows);
+        callback(data.rows);
+    })
+}
+
+module.exports = {getTopics};
